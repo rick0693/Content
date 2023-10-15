2c7c30c66fa4e4035d708b353e18ea1c4fcc3860
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-10-15 15:36:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -22,9 +22,6 @@ class ConsultaNotas:
         self.dados_login_empresa = dados_login_empresa
 
 
-
-
-
     def extrair_data_especifica(self,soup):
         # Encontrar todos os elementos <p> com a classe 'tdb'
         elementos_tdb = soup.find_all('p', {'class': 'tdb'})
@@ -84,36 +81,47 @@ class ConsultaNotas:
             soup = BeautifulSoup(response.text, 'html.parser')
             info_block = soup.find('tr', {'style': 'background-color:#FFFFFF;cursor:pointer;'})
 
-            if info_block:
-                situacao_element = info_block.find('p', {'class': 'titulo'})
-                nf_element = info_block.find('p', {'class': 'tdb'})
 
-                if situacao_element and nf_element:
-                    situacao_text = situacao_element.get_text(strip=True)
+        if info_block:
+            situacao_element = info_block.find('p', {'class': 'titulo'})
+            nf_element = info_block.find('p', {'class': 'tdb'})
 
-                    # Remover conteúdo entre parênteses
-                    situacao_text = re.sub(r'\([^)]*\)', '', situacao_text)
+            if situacao_element and nf_element:
+                situacao_text = situacao_element.get_text(strip=True)
+                situacao_text = re.sub(r'\([^)]*\)', '', situacao_text)
+                nf_text = nf_element.get_text(strip=True)
+                data_situacao = self.extrair_data_especifica(soup)
 
-                    nf_text = nf_element.get_text(strip=True)
-                    data_situacao = self.extrair_data_especifica(soup)
+                # Atualize a coluna 'DATA ENTREGA' se a situação for "MERCADORIA ENTREGUE"
+                if "MERCADORIA ENTREGUE" in situacao_text:
+                    df.loc[df['Nro. Nota'] == numero_nota, 'DATA ENTREGA'] = data_situacao
 
-                    # Se a situação for "MERCADORIA ENTREGUE", então atualize a coluna 'DATA ENTREGA'
-                    if "MERCADORIA ENTREGUE" in situacao_text:
-                        df.loc[df['Nro. Nota'] == numero_nota, 'DATA ENTREGA'] = data_situacao
+                    # Atualize a coluna 'SITUAÇÃO DA ENTREGA' com base nas condições fornecidas
+                    df.loc[df['Nro. Nota'] == numero_nota, 'SITUAÇÃO DA ENTREGA'] = self.atualizar_situacao_entrega(df, numero_nota)
 
-                    df.loc[df['Nro. Nota'] == numero_nota, 'STATUS'] = situacao_text
+                df.loc[df['Nro. Nota'] == numero_nota, 'STATUS'] = situacao_text
 
-                    # Exibir o DataFrame atualizado após cada consulta
-                    dataframe_atualizado.dataframe(df.tail(100000)) 
-
-                else:
-                    st.write(f'Elementos de situação, NF ou data não encontrados para {nome_tabela} - Nota {numero_nota}')
+                # Exibir o DataFrame atualizado após cada consulta
+                dataframe_atualizado.dataframe(df.tail(100000000))
             else:
-                st.write(f'Bloco de informações não encontrado para {nome_tabela} - Nota {numero_nota}')
+                st.write(f'Elementos de situação, NF ou data não encontrados para {nome_tabela} - Nota {numero_nota}')
         else:
-            st.write(f'Erro no login para {nome_tabela} - Nota {numero_nota}')
+            st.write(f'Bloco de informações não encontrado para {nome_tabela} - Nota {numero_nota}')
 
+    def atualizar_situacao_entrega(self, df, numero_nota):
+        # Função para atualizar a coluna 'SITUAÇÃO DA ENTREGA'
+        previsao_entrega = df.loc[df['Nro. Nota'] == numero_nota, 'PREVISÃO DE ENTREGA'].values[0]
+        data_entrega = df.loc[df['Nro. Nota'] == numero_nota, 'DATA ENTREGA'].values[0]
 
+        if pd.notna(data_entrega):
+            if data_entrega > previsao_entrega:
+                return "ENTREGUE FORA DO PRAZO"
+            else:
+                return "ENTREGUE NO PRAZO"
+        elif pd.notna(previsao_entrega) and previsao_entrega < datetime.now().strftime('%d/%m/%Y'):
+            return "EM TRANSITO ATRASADO"
+        else:
+            return "EM TRANSITO"
 
     def atualizar_colunas(self, df):
         # Atualizando a coluna 'MÊS' com base na coluna 'Data de Saída'
@@ -136,7 +144,6 @@ class ConsultaNotas:
             'AC': 'NORTE',
             'AL': 'NORDESTE',
             'AP': 'NORTE',
-
             'AM': 'NORTE',
             'BA': 'NORDESTE',
             'CE': 'NORDESTE',
@@ -191,9 +198,9 @@ dados_login_empresa = {
         'cnpj': '07117654000149',
         'senha': 'MAIORALT',
     },
-    'FITLOG TRANSPORTES E LOGISTICA': {
+    'TENHOMOVEIS COMERCIO DE MOVEIS E UTENSILIOS DOMESTICOS LTDA': {
         'cnpj': '07117654000149',
-        'senha': 'FOTUS23',
+        'senha': ' ',
     },
     'CT DISTRIBUICAO E LOGISTICA LTDA': {
         'cnpj': '07117654000149',
